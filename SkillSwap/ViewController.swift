046c8141339660de6e2e72d7f1452439a312231a
George Bonnici-Carter <george.bonnici@zoocha.com>
2017-07-22 10:31:33 +0100

    Initial Commit

---
+++ SkillSwap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SkillSwap
+//
+//  Created by George Bonnici-Carter on 22/07/2017.
+//  Copyright © 2017 George Bonnici-Carter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
